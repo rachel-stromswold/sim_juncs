fixed dropped terms in Hessian

scripts/phase_plot_new.py
@@ -220,7 +220,7 @@ class cluster_reader:
 
         #try loading precomputed data
         data_name = "{}/fit_data_{}".format(prefix, fname.split('/')[-1].split('.')[0])
-        data_shape = (len(self.clust_names), clust_len, phases.HERM_OFF+herm_n+ang_n)
+        data_shape = (len(self.clust_names), clust_len, NX)
         if not recompute and os.path.exists(data_name):
             print("found cluster data ", data_name)
             with open(data_name, 'rb') as dat_f:
@@ -402,7 +402,7 @@ if args.point == '':
     plt.close(rs_fig)
 else:
     cr = cluster_reader(args.fname[0], args.herm_n, args.ang_n, use_prior=args.use_prior, prefix=args.prefix, recompute=args.recompute, save_fit_figs=args.save_fit_figs)
-    #phases.signal._do_grad_tests(np.array([0.4]), 3, 2)
+    phases.signal._do_grad_tests(np.array([0.4]), 3, 2)
     point_arr = args.point.split(",")
     clust = "cluster_"+point_arr[0]
     j = int(point_arr[1])

scripts/phases_new.py
@@ -168,11 +168,12 @@ class signal:
         nf = freqs.shape[0]
         fs = np.tile(freqs, 2).reshape((2,nf))
         sign_dw = np.meshgrid(np.ones(nf), np.array([-1,1]))[1]
-        dw = fs[:,:] + sign_dw*x[2]
         #calculate arguments
         n = x.shape[0]
-        eargs = -sign_dw*x[0] - fs*x[1]
+
         ang_off = HERM_OFF+herm_n
+        dw = fs[:,:] + sign_dw*x[2]
+        eargs = -sign_dw*x[0] - fs*x[1]
         for m in range(ang_n):
             eargs += x[ang_off+m]*dw**(2*m+3)
         #eargs *= 2*np.pi
@@ -189,8 +190,7 @@ class signal:
                 ang_grads[:,2,:] += sign_dw*(2*m+3)*x[m+ang_off]*dw**(2*m+2)
                 ang_hess[:,ang_off+m,2,:] = sign_dw*(2*m+3)*dw**(2*m+2)
                 ang_hess[:,2,ang_off+m,:] = ang_hess[:,ang_off+m,2,:]
-                if m > 0:
-                    ang_hess[:,2,2,:] += (2*m+3)*(2*m+2)*x[m+ang_off]*dw**(2*m+1)
+                ang_hess[:,2,2,:] += (2*m+3)*(2*m+2)*x[m+ang_off]*dw**(2*m+1)
             #calculate magnitude gradients
             dw *= x[3]
             mag_grads = np.zeros((2, n, nf))